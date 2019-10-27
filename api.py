2c4944e2c7ab0b37729ed0fddd1c70b632c81633
Edward Li <edwardyaoli@gmail.com>
2019-10-26 20:42:44 -0400

    added ml analysis and threading

--- api.py
+++ api.py
@@ -4,38 +4,84 @@ import sqlite3
 from ml.daybookml.analysis import analyze_journal
 from ml.daybookml.summary import top_emotion_effectors
 
+import threading
+
 app = Flask(__name__)
 
 conn = sqlite3.connect('database.db')
 #conn.execute("DROP TABLE posts")
 #conn.execute("DROP TABLE sentiments")
-conn.execute("CREATE TABLE posts (Id INTEGER PRIMARY KEY AUTOINCREMENT, journal TEXT, user TEXT, sentiment_score DECIMAL, sentiment_magnitude DECIMAL, sleep INTEGER, wake INTEGER, sleepTime INTEGER)")
+conn.execute("CREATE TABLE posts (id INTEGER PRIMARY KEY AUTOINCREMENT, journal TEXT, user TEXT, sentiment DECIMAL, sleep INTEGER, wake INTEGER, sleepTime INTEGER)")
 #"category" below refer to event, people, location, other
-conn.execute("CREATE TABLE sentiments (Id INTEGER, category TEXT, word TEXT, sentiment_score DECIMAL, sentiment_magnitude DECIMAL)")
+conn.execute("CREATE TABLE sentiments (id INTEGER, category TEXT, word TEXT, sentiment DECIMAL, sentiment_magnitude DECIMAL)")
 
-@app.route('/makeJournal', methods = ['POST'])
-def makeJournal():
-    result = request.json
+def run_sentiment(journal_text, journal_id):
+    print("[INFO] running sentiment")
+    sentiment, entity_dict = analyze_journal(journal_text)
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    command = f"UPDATE posts SET sentiment = {sentiment} WHERE id = {journal_id}"
+    cursor.execute(command)
+    conn.commit()
+
+    for category in entity_dict:
+        for entity in entity_dict[category]:
+            command = f"INSERT INTO sentiments (id, category, word, sentiment_score, sentiment_magnitude) VALUES ({journal_id}, \"{category}\", \"{entity[0]}\", {entity[1]}, {entity[2]})"
+            cursor.execute(sentimentCommand)
+            conn.commit()
+
+    print(f"[INFO] Done running sentiment for journal {journal_id}")
+
+def makeJournal(result):
     journal = result["journal"]
     user = result["user"]
-    sentiment_score = result["sentiment_score"]
-    sentiment_magnitude = result["sentiment_magnitude"]
-    sentiment = analysis
+    sentiment = 0 # We'll spawn a thread to fix this soon.
     sleep = result["sleep"]
     wake = result["wake"]
     sleepTime = calcSleep(sleep, wake)
+
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
-    command = "INSERT INTO posts (journal, user, sentiment_score, sentiment_magnitude, sleep, wake, sleepTime) VALUES (\"%s\",\"%s\",%d,%d,%d,%d,%d)" %(journal,user,sentiment_score,sentiment_magnitude,sleep,wake,sleepTime)
+    command = "INSERT INTO posts (journal, user, sentiment, sleep, wake, sleepTime) VALUES (\"%s\",\"%s\",%d,%d,%d,%d,%d)" %(journal, user, sentiment, sleep, wake, sleepTime)
     cursor.execute(command)
-    Id = result["Id"]
-    category = result["category"]
-    word = result["word"]
-    sentimentCommand = "INSERT INTO sentiments (Id, category, word, sentiment_score, sentiment_magnitude) VALUES (%d,\"%s\",\"%s\",%f,%f)" %(Id, category, word, sentiment_score, sentiment_magnitude)
-    cursor.execute(sentimentCommand)
-    lastId = cursor.lastrowid
-    conn.commit()
-    return sentimentCommand
+    journal_id = cursor.lastrowid
+
+    sent_thr = threading.Thread(target=run_sentiment, args=(result["journal"], journal_id,))
+    sent_thr.start()
+    return jsonify(journal_id)
+
+def updateJournal():
+    journal_id = result["id"]
+    journal = result["journal"]
+    sleep = result["sleep"]
+    wake = result["wake"]
+    sleepTime = calcSleep(sleep, wake)
+
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    try:
+        command = f"UPDATE posts SET journal = \"{journal}\", sleep = {sleep}, wake = {wake}, sleepTime = {sleepTime} WHERE id = {journal_id}"
+        cursor.execute(command)
+        conn.commit()
+
+        command = f"DELETE FROM sentiments WHERE id = {journal_id}"
+        cursor.execute(command)
+        conn.commit()
+
+        sent_thr = threading.Thread(target=run_sentiment, args=(result["journal"], journal_id,))
+        sent_thr.start()
+        return jsonify(0)
+    except:
+        return jsonify(-1)
+
+@app.route('/updateJournal', methods = ['POST'])
+def _make_update_journal():
+    result = request.json
+    if result['id'] == 0:
+        return makeJournal(request)
+    else:
+        return updateJournal(request)
 
 #based on date range
 @app.route('/getJournal', methods = ['GET'])
@@ -43,23 +89,6 @@ def getJournal():
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
     return jsonify(cursor.execute("SELECT * FROM sentiments LIMIT 10").fetchall())
-
-@app.route('/updateJournal', methods = ['POST'])
-def updateJournal():
-    result = request.json
-    journal = result["journal"]
-    user = result["user"]
-    sentiment_score = 0
-    sentiment_magnitude = 0
-    #sentiment = analysis
-    sleep = result["sleep"]
-    wake = result["wake"]
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    command = "UPDATE posts (journal, user, sentiment_score, sentiment_magnitude, sleep, wake) VALUES (\"%s\",\"%s\",%d,%d,%d,%d)" %(journal,user,sentiment_score,sentiment_magnitude,sleep,wake)
-    cursor.execute(command)
-    conn.commit()
-    return command
 
 def calcSleep(sleep, wake):
     if sleep == wake:
