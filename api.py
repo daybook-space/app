89bac71181aa9fffdebed55ec8ed9a3c79960b67
Edward Li <edwardyaoli@gmail.com>
2019-10-27 01:07:48 -0400

    changed port

--- api.py
+++ api.py
@@ -193,4 +193,4 @@ def calcSleep(sleep, wake):
 
 if __name__ == '__main__':
    #app.EXPLAIN_TEMPLATE_LOADING = True
-   app.run(host='0.0.0.0', port=80, debug=True)
+   app.run(host='0.0.0.0', port=5000, debug=True)
